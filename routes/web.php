50070bc0c505a7cbb2d3913d9cbbd25213fcb14b
Sander Jochems <sanderjochems@hotmail.nl>
2020-03-17 15:59:37 +0100

    Create Controllers & Routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('orders', 'OrderController');
+
+Route::resource('customers', 'CustomerController');
+
+Route::resource('products', 'ProductController');
