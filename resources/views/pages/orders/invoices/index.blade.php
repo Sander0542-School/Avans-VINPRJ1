a5375e04db82f9e4f1872eb9e7ba8dbbdd25cedf
Tommy Hosewol <t.hosewol@gmail.com>
2020-03-19 22:21:06 +0100

    Invoicing

--- resources/views/pages/orders/invoices/index.blade.php
+++ resources/views/pages/orders/invoices/index.blade.php
@@ -1,15 +1,37 @@
-@extends('layouts.app', ['title' => 'Order Invoices'])
+@extends('layouts.app', ['title' => 'Facturen'])
 
 @section('content')
     <div class="container">
-        <h1 class="text-center">Order Invoices</h1>
+        <div class="row text-center">
+            <div class="col-4 offset-2">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Bestellingen aanmaken</h4>
+                        <a href="{{ route('orders.create') }}" class="btn btn-primary">Openen</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Bestellingen weergeven</h4>
+                        <a href="{{ route('orders.index') }}" class="btn btn-primary">Openen</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <hr />
+
+        <h1 class="text-center">Facturen</h1>
         <table class="table table-striped table-hover" id="invoices">
             <thead class="thead-dark">
             <tr>
-                <th scope="col">Order</th>
-                <th scope="col">Customer</th>
-                <th scope="col">Total price</th>
-                <th scope="col">Paid?</th>
+                <th scope="col">Bestelling</th>
+                <th scope="col">Klant</th>
+                <th scope="col">Totaalprijs</th>
+                <th scope="col">Betaald?</th>
+                <th scope="col">Openen</th>
             </tr>
             </thead>
             <tbody>
@@ -18,17 +40,18 @@
                     <tr class="clickable" onclick="">
                         <th scope="row">{{ $invoice->order->id }}</th>
                         <td>{{ $invoice->order->customer->name }}</td>
-                        <td>&euro; NULL</td>
+                        <td>@money($invoice->order->total)</td>
                         @if ($invoice->paid)
-                            <td class="text-success">Yes</td>
+                            <td class="text-success">Ja</td>
                         @else
-                            <td class="text-danger">No</td>
+                            <td class="text-danger">Nee</td>
                         @endif
+                        <td><a href="{{ route('orders.show', $invoice->order) }}" class="btn btn-primary">&rArr;</a></td>
                     </tr>
                 @endforeach
             @else
                 <tr>
-                    <td colspan="8">There are no order with an invoice available!</td>
+                    <td colspan="8">Er zijn geen facturen beschikbaar!</td>
                 </tr>
             @endif
             </tbody>
