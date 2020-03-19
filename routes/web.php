79873b99bfc90c58c507dbe7487c653022bb0edc
Sander Jochems <sanderjochems@hotmail.nl>
2020-03-19 12:32:32 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('', 'HomeController@index')->name('home.index');
 
-Route::resource('orders', 'OrderController');
+Route::post('orders/{order}/products', 'OrderController@storeProduct')->name('orders.products.store');
+Route::put('orders/{order}/products/{product}', 'OrderController@updateProduct')->name('orders.products.update');
+Route::delete('orders/{order}/products/{product}', 'OrderController@destroyProduct')->name('orders.products.destroy');
+Route::resource('orders', 'OrderController')->except(['edit', 'update']);
 
 Route::resource('customers', 'CustomerController');
 
