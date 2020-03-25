185b22d3dbe40070f6e0fc0b05e72f14270c5fa1
Aremyre | Lars <33829613+Aremyre@users.noreply.github.com>
2020-03-25 14:13:38 +0100

    Update resources/views/pages/customers/create.blade.php
    Co-Authored-By: Sander Jochems <sanderjochems@hotmail.nl>

--- resources/views/pages/customers/create.blade.php
+++ resources/views/pages/customers/create.blade.php
@@ -9,7 +9,7 @@
                     <div class="card">
                         <div class="card-body">
                             <h4 class="card-title text-center">Klant toevoegen</h4>
-                            <label for="addressCity">Bedrijf naam</label>
+                            <label for="addressCity">Naam</label>
                             <input type="text" name="name" class="form-control" id="bedrijf_naam">
     
                             <input type="submit" class="btn btn-info mt-2" value="aanmaken"/>
