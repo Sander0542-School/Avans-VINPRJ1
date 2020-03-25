849686f2144964cd44cc4965fbbef7608274ab84
Aremyre | Lars <33829613+Aremyre@users.noreply.github.com>
2020-03-25 14:20:32 +0100

    Update resources/views/pages/customers/contacts/show.blade.php
    Co-Authored-By: Sander Jochems <sanderjochems@hotmail.nl>

--- resources/views/pages/customers/contacts/show.blade.php
+++ resources/views/pages/customers/contacts/show.blade.php
@@ -10,13 +10,13 @@
                     <div class="card">
                         <div class="card-body">
                             <h4 class="card-title text-center">Contact bewerken</h4>
-                            <label for="contactName">Contact naam</label>
+                            <label for="contactName">Naam</label>
                             <input type="text" name="name" class="form-control" id="contactName" value="{{$contact->name}}">
-                            <label for="contactEmail">Contact email</label>
+                            <label for="contactEmail">Email</label>
                             <input type="text" name="email" class="form-control" id="contactEmail" value="{{$contact->email}}">
-                            <label for="contactPhone">Contact telefoon nummer</label>
+                            <label for="contactPhone">Telefoon</label>
                             <input type="text" name="phone" class="form-control" id="contactPhone" value="{{$contact->phone}}">
-                            <label for="contactTitle">Contact werk titel</label>
+                            <label for="contactTitle">Functietitel</label>
                             <input type="text" name="jobtitle" class="form-control" id="contactTitle" value="{{$contact->jobtitle}}">
     
                             <input type="submit" class="btn btn-info mt-2" value="Opslaan"/>
