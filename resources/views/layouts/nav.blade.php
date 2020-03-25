4a700596887d5eb543a44cf77170573cc8117bb8
Sander Jochems <sanderjochems@hotmail.nl>
2020-03-25 13:35:36 +0100

    Add link to nav

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -18,6 +18,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('products.index') }}">{{ __('Producten') }}</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('suppliers.index') }}">{{ __('Leveranciers') }}</a>
+                </li>
             </ul>
         </div>
     </div>
