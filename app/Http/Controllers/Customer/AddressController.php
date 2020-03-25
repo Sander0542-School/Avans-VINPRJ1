4a56011173d0445142085c09c420b75d3ed05518
Aremyre | Lars <33829613+Aremyre@users.noreply.github.com>
2020-03-25 14:08:35 +0100

    Update app/Http/Controllers/Customer/AddressController.php
    Co-Authored-By: Sander Jochems <sanderjochems@hotmail.nl>

--- app/Http/Controllers/Customer/AddressController.php
+++ app/Http/Controllers/Customer/AddressController.php
@@ -10,16 +10,6 @@ use App\Models\Customer;
 class AddressController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
