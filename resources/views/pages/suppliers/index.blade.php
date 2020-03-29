cc76a3e2e93b68447cedad3769bcad8771014dc8
Sander Jochems <sanderjochems@hotmail.nl>
2020-03-29 12:26:41 +0200

    Update

--- resources/views/pages/suppliers/index.blade.php
+++ resources/views/pages/suppliers/index.blade.php
@@ -2,19 +2,6 @@
 
 @section('content')
     <div class="container">
-        <div class="row text-center">
-            <div class="col-8 offset-2">
-                <div class="card">
-                    <div class="card-body">
-                        <h4 class="card-title">Leverancier toevoegen</h4>
-                        <a href="{{ route('suppliers.create') }}" class="btn btn-primary">Openen</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <hr/>
-
         <div class="row">
             <div class="col-12">
                 <h1 class="text-center">Leveranciers</h1>
